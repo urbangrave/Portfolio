9ab757674cbb83e8e4c49edb8026477167e488f0
urbangrave22 <urbangrave22@gmail.com>
2024-01-22 20:00:04 +0800

    Updates

---
+++ precache-manifest.6e77716a0bcd810c358587f9ef547b37.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c54112ace16e5e16b64a5c55e95b103",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "0841444bb7e9a17bbf5b",
+    "url": "/Portfolio/static/css/2.5175cca5.chunk.css"
+  },
+  {
+    "revision": "88a8640ce3a5976f447b",
+    "url": "/Portfolio/static/css/main.bda03402.chunk.css"
+  },
+  {
+    "revision": "0841444bb7e9a17bbf5b",
+    "url": "/Portfolio/static/js/2.6f0927a9.chunk.js"
+  },
+  {
+    "revision": "88a8640ce3a5976f447b",
+    "url": "/Portfolio/static/js/main.7044c04e.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "59867bee3e6ac4fddb1f1080b0142ca8",
+    "url": "/Portfolio/static/media/BMS.59867bee.png"
+  },
+  {
+    "revision": "c0a91dd0101a586d54fdfbef75be2aab",
+    "url": "/Portfolio/static/media/Bookkeeping.c0a91dd0.png"
+  },
+  {
+    "revision": "d279ff632b01979c200c2da0da8bdd4e",
+    "url": "/Portfolio/static/media/Dts.d279ff63.png"
+  },
+  {
+    "revision": "ff61d2860bde30a53cf695af9664ddd7",
+    "url": "/Portfolio/static/media/GH.ff61d286.svg"
+  },
+  {
+    "revision": "f780c213eccff630a5a58930f962f5f4",
+    "url": "/Portfolio/static/media/Psds.f780c213.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Portfolio/static/media/defaultNavLogo.06e73328.svg"
+  },
+  {
+    "revision": "6c7d72e66a546a8ca8c54c5956becb50",
+    "url": "/Portfolio/static/media/hero-dark.6c7d72e6.jpg"
+  },
+  {
+    "revision": "4cb9e4a324c6803f7ae1d160a712bbd1",
+    "url": "/Portfolio/static/media/hero-light.4cb9e4a3.jpg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Portfolio/static/media/logo.06e73328.svg"
+  }
+]);
